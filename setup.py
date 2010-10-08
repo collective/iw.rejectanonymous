675f4d06e6be6bf44b24eb8ffcb53fb89738334c
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-10-08 10:03:43 +0000

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.1'
+version = '1.0.2dev'
 
 README = os.path.join(os.path.dirname(__file__),
                       'iw', 'rejectanonymous', 'docs', 'README.txt')
