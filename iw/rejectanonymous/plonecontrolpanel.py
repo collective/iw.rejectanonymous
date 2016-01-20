90a28da8c60dca8d1eaf59f0ef4342da766d8d84
esteele <eric@esteele.net>
2016-01-20 15:32:12 -0500

    PEP8

--- iw/rejectanonymous/plonecontrolpanel.py
+++ iw/rejectanonymous/plonecontrolpanel.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-## Copyright (C) 2008 Ingeniweb
+# Copyright (C) 2008 Ingeniweb
 
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-## You should have received a copy of the GNU General Public License
-## along with this program; see the file COPYING. If not, write to the
-## Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """Patch plone "security" control panel to add a new option
 """
-from zope.interface import (
-    implementedBy, classImplementsOnly, alsoProvides, noLongerProvides
-    )
+from zope.interface import implementedBy
+from zope.interface import classImplementsOnly
+from zope.interface import alsoProvides
+from zope.interface import noLongerProvides
 from zope.schema import Bool
 from zope.component import getGlobalSiteManager
 from zope.formlib.form import FormFields
-
 
 from plone.app.controlpanel.security import SecurityControlPanel
 from plone.app.controlpanel.security import SecurityControlPanelAdapter
@@ -37,13 +37,16 @@ class IPrivateSiteSchema(ISecuritySchema):
         description=u"Users must login to view the site. Anonymous users are presented the login form",
         default=False,
         required=False,
-        )
+    )
 
 # add accessors to adapter
+
+
 def get_private_site(self):
     return IPrivateSite.providedBy(self.portal)
 
 SecurityControlPanelAdapter.get_private_site = get_private_site
+
 
 def set_private_site(self, value):
     operator = value and alsoProvides or noLongerProvides
@@ -54,7 +57,7 @@ SecurityControlPanelAdapter.set_private_site = set_private_site
 SecurityControlPanelAdapter.private_site = property(
     SecurityControlPanelAdapter.get_private_site,
     SecurityControlPanelAdapter.set_private_site
-    )
+)
 
 # re-register adapter with new interface
 _decl = implementedBy(SecurityControlPanelAdapter)
